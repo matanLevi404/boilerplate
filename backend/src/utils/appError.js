b69eb44054c6271aa01de27f460c1b62aa354d66
matanLevi404 <matan.levi.404@gmail.com>
2024-02-27 18:48:23 +0200

    feat/added-error-middleware

---
+++ backend/src/utils/appError.js
@@ -0,0 +1,22 @@
+class AppError extends Error {
+    name;
+    httpCode;
+    isOperational;
+    extraDetails;
+
+    constructor({name, httpCode = 500, description, isOperational = false, extraDetails = null}) {
+        super(description)
+
+        Object.setPrototypeOf(this, new.target.prototype);
+
+        this.name= name;
+        this.httpCode = httpCode;
+        this.isOperational = isOperational;
+        this.description = description;
+        this.extraDetails = extraDetails;
+        
+        Error.captureStackTrace(this);
+    }
+}
+
+export default AppError
